eee2f518ab9c0c4d693f9ad224a8f7b75c298015
AvtoG <agrigolia@cca.edu>
2017-02-22 01:09:18 -0800

    Create sketch-2.js

---
+++ sketch-2.js
@@ -0,0 +1,13 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+  background(220);
+	
+	for (x=0; x<400; x+=10) {
+		for(y=0; y<400; y+=10) {
+			ellipse(x,y,10,10)
+		}
+	}
+}
